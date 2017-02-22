755371d9f8e1f0939a56fb4d6fc503e135718ea4
kuanawanda <aaron.t.kuan@gmail.com>
2017-02-22 11:07:52 -0500

    More module changes - notebook now runs all the way through.

--- code/read_tiff_files.py
+++ code/read_tiff_files.py
@@ -7,14 +7,12 @@ def read_tiff_files(files_location):
     files.sort()
     input_data = []
     file_count = 0
-    
+
     for file in files:
         input_data.append(np.asarray(Image.open(file)))
         file_count += 1
-        
+
     input_data = np.asarray(input_data)
-    # input data axes is z,y,x - change it to x,y,z                                                     
+    # input data axes is z,y,x - change it to x,y,z
     input_data = np.transpose(input_data, (2,1,0))
     return input_data
-
-
